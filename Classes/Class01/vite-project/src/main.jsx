185e138978a326a2489f00797e594a3023e6142f
coder-ali-ba <habibali8770@gmail.com>
2025-04-24 23:12:06 +0500

    comp

--- Classes/Class01/vite-project/src/main.jsx
+++ Classes/Class01/vite-project/src/main.jsx
@@ -1,22 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {App , Header , Body} from './App.jsx'
+import './App.css'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
 
-//   </StrictMode>,
-// )
 
 const root = createRoot(document.getElementById("root"))
 root.render(
-  // <StrictMode>
-  //     <h1>habib ali</h1>
-  
-  // </StrictMode>,
-  <div><h1>habib ali</h1>
-  <img  src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg" alt="" /></div>
+  <div className='main'>
+        <h1>habib ali</h1>
+        <img  src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg" alt="" />
+        <App/>
+        <Header />
+        <Body />
+  </div>
   
 )
